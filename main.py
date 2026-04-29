1b5e25932cdaa1dd2693dba01e1935fc79d42bd1
vishugni <vishushakyaxydream@gmail.com>
2026-04-29 14:18:18 +0530

    sadasdsa

--- main.py
+++ main.py
@@ -38,4 +38,4 @@ def chrome_devtools_config():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
